1356377e8fcd370aa4b060eb30698542c2fcf396
lyy16 <lvyueying@8lab.com>
2024-06-18 11:24:09 +0800

    env&gitnore

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -7,7 +7,7 @@ import { PortisConnector } from '@web3-react/portis-connector'
 import { FortmaticConnector } from './Fortmatic'
 import { NetworkConnector } from './NetworkConnector'
 
-const NETWORK_URL = process.env.REACT_APP_NETWORK_URL
+const NETWORK_URL = `${window.location.origin}${process.env.REACT_APP_NETWORK_URL}`
 const FORMATIC_KEY = process.env.REACT_APP_FORTMATIC_KEY
 const PORTIS_ID = process.env.REACT_APP_PORTIS_ID
 
@@ -27,7 +27,7 @@ export function getNetworkLibrary(): Web3Provider {
 }
 
 export const injected = new InjectedConnector({
-  supportedChainIds: [1, 3, 4, 5, 42,345]
+  supportedChainIds: [1, 3, 4, 5, 42, 345]
 })
 
 // mainnet only
